077ded8c9a9877d7d0fb2263b432e170eca634f6
JoshuaRiches <j.riches73@outlook.com>
2021-03-19 14:55:41 +0000

    doesnt move when in view now :)

--- CT4105 Escape Room Game/Assets/Scripts/Jaakko.cs
+++ CT4105 Escape Room Game/Assets/Scripts/Jaakko.cs
@@ -11,25 +11,20 @@ public class Jaakko : MonoBehaviour
 
     void Update()
     {
+        Vector3 screenPoint = Player.GetChild(0).GetComponent<Camera>().WorldToViewportPoint(gameObject.transform.position);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        if (onScreen) {
+            Speed = 0f;
+        }
+        else {
+            Speed = 5f;
+        }
+
         Enemy.transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
         float step = Speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, Player.position, step);
-    }
 
-    void OnTriggerEnter(Collider Coll)
-    {
-        if (Coll.gameObject.tag == "Looking")
-        {
-            Speed = 0;
-        }
-    }
 
-    void OnTriggerExit(Collider Coll)
-    {
-        if (Coll.gameObject.tag == "Looking")
-        {
-            transform.LookAt(Player);
-            Speed = 5f;
-        }
     }
 }
