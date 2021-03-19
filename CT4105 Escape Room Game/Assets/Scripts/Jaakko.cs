84def53a6c54ed10e168b81795ffdc7cb0d44920
MLDanny <67206020+MLDanny@users.noreply.github.com>
2021-03-19 14:18:07 +0000

    Jaakko Changes

---
+++ CT4105 Escape Room Game/Assets/Scripts/Jaakko.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Jaakko : MonoBehaviour
+
+{
+
+    public GameObject Enemy;
+    public Transform Player;
+    public float Speed;
+
+    void Update()
+    {
+        Enemy.transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+        float step = Speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Player.position, step);
+    }
+
+    void OnTriggerEnter(Collider Coll)
+    {
+        if (Coll.gameObject.tag == "Looking")
+        {
+            Speed = 0;
+        }
+    }
+
+    void OnTriggerExit(Collider Coll)
+    {
+        if (Coll.gameObject.tag == "Looking")
+        {
+            transform.LookAt(Player);
+            Speed = 5f;
+        }
+    }
+}
---
+++ CT4105 Escape Room Game/Assets/Scripts/SightLine.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+
+            GameObject Jaakko = hit.transform.gameObject;
+
+            if (Jaakko.tag == "Jaakko")
+            {
+                Jaakko.GetComponent<Jaakko>().Speed = 0f;
+            }
+            else
+            {
+                Jaakko.GetComponent<Jaakko>().Speed = 5f;
+            }
+        }
+        else
+        {
+            GameObject Jaakko = hit.transform.gameObject;
+            Jaakko.GetComponent<Jaakko>().Speed = 5f;
+        }
+    }
+}
