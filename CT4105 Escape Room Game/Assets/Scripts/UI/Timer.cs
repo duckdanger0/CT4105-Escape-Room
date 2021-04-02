91b09c219919711404cb913f3c262015acca1fc9
MLDanny <67206020+MLDanny@users.noreply.github.com>
2021-04-02 14:29:33 +0100

    Maze Styling

---
+++ CT4105 Escape Room Game/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private string newLevel;
+    public float timer = 30f;
+
+    int seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        seconds = 0;
+
+        timer += 1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timer -= Time.deltaTime;
+
+        seconds = ( int )( timer );
+
+        if ( seconds < 1 )
+        {
+
+            SceneManager.LoadScene(newLevel);
+
+        }
+
+    }
+
+}
