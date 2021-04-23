02d05b6913793b5c48429a8077d82ff0f7a86d0f
duckdanger0 <77931413+duckdanger0@users.noreply.github.com>
2021-04-23 16:40:29 +0100

    NewMiniGame

---
+++ CT4105 Escape Room Game/Assets/LightSwitch/Switch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject up;
+    public GameObject on;
+    public GameObject down;
+    public GameObject off;
+    public bool switchValue;
+
+    void Update()
+    {
+
+        if (switchValue == true)
+        {
+            on.SetActive(true);
+            up.SetActive(true);
+            down.SetActive(false);
+            off.SetActive(false);
+        }
+        else
+        {
+            on.SetActive(false);
+            up.SetActive(false);
+            down.SetActive(true);
+            off.SetActive(true);
+        }
+    }
+    void OnMouseDown()
+    {
+        switchValue = !switchValue;
+    }
+}
+
