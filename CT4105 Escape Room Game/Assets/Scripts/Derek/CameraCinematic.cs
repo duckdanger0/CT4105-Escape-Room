a995c4fe64a94fcc66c77e0d65278bde3d1e0d1c
MLDanny <67206020+MLDanny@users.noreply.github.com>
2021-05-09 19:08:06 +0100

    Derek Changes

---
+++ CT4105 Escape Room Game/Assets/Scripts/Derek/CameraCinematic.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCinematic : MonoBehaviour {
+
+
+    [SerializeField]
+    private Cinematic rail;
+
+    private int currentSegment = 1;
+    private float transition;
+    private bool isCompleted;
+
+    public PlayMode mode;
+    public float speed = 2.5f;
+    public bool isReversed;
+    public bool isLooping;
+    public bool isToAndFro;
+
+    float timer, cooldownTimer;
+    bool timerActive, coolDownActive, movementEnabled;
+
+    void Start(){
+        timer = 1f;
+        cooldownTimer = 1f;
+        timerActive = true;
+        movementEnabled = true;
+        coolDownActive = false;
+    }
+
+
+    private void Update() {
+        if (!rail) {
+            return;
+        }
+        if (!isCompleted) {
+            Play(!isReversed);
+        }
+
+    }
+
+    private void Play(bool forward = true) {
+
+            float magnitude = (rail.nodes[currentSegment + 1].position - rail.nodes[currentSegment].position).magnitude;
+            float cameraSpeed = (Time.deltaTime * 1 / magnitude) * speed;
+            transition += (forward) ? cameraSpeed : -cameraSpeed;
+
+            if (transition > 1) {
+                transition = 0;
+                currentSegment++;
+
+                if (currentSegment == rail.nodes.Length - 1) {
+                    if (isLooping) {
+                        if (isToAndFro) {
+                            transition = 1;
+                            currentSegment = rail.nodes.Length - 2;
+                            isReversed = !isReversed;
+                        }
+                        currentSegment = 1;
+                    }
+                    else {
+                        isCompleted = true;
+                        return;
+                    }
+                }
+            }
+            else if (transition < 0) {
+                transition = 1;
+                currentSegment--;
+
+                if (currentSegment == -1) {
+
+                    if (isLooping) {
+
+                        if (isToAndFro) {
+                            transition = 1;
+                            currentSegment = 1;
+                            isReversed = !isReversed;
+                        }
+                        currentSegment = rail.nodes.Length - 2;
+                    }
+                    else {
+                        isCompleted = true;
+                        return;
+                    }
+                }
+            }
+
+        transform.position = rail.PosOnRail(currentSegment, transition, mode);
+        //transform.rotation = rail.Orientation(currentSegment, transition);
+        transform.rotation = rail.CurrentSegmentAngle(currentSegment);
+    }
+}
---
+++ CT4105 Escape Room Game/Assets/Scripts/Derek/Cinematic.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayMode
+{
+    linear,
+    catmull,
+}
+public class Cinematic : MonoBehaviour {
+
+    public Transform[] nodes;
+
+    private void Start() {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+
+    public Vector3 PosOnRail(int segment, float ratio, PlayMode mode) {
+        switch (mode) {
+            case PlayMode.linear:
+                return LinearPos(segment, ratio);
+
+            case PlayMode.catmull:
+                return CatmullPos(segment, ratio);
+
+            default:
+                return LinearPos(segment, ratio);
+        }
+    }
+
+    public Vector3 LinearPos(int segment, float ratio) {
+        Vector3 pos1 = nodes[segment].position;
+        Vector3 pos2 = nodes[segment + 1].position;
+
+        return Vector3.Lerp(pos1, pos2, ratio);
+    }
+
+    public Quaternion Orientation(int segment, float ratio) {
+        Quaternion quat1 = nodes[segment].rotation;
+        Quaternion quat2 = nodes[segment + 1].rotation;
+
+        return Quaternion.Lerp(quat1, quat2, ratio);
+    }
+
+    public Vector3 CatmullPos(int segment, float ratio) {
+        Vector3 p1, p2, p3, p4;
+
+        if (segment == 1) {
+            p1 = nodes[segment].position;
+            p2 = p1;
+            p3 = nodes[segment + 1].position;
+            p4 = nodes[segment + 2].position;
+        }
+        else if (segment == nodes.Length - 2) {
+            p1 = nodes[segment - 1].position;
+            p2 = nodes[segment].position;
+            p3 = nodes[segment + 1].position;
+            p4 = p3;
+        }
+        else {
+            p1 = nodes[segment - 1].position;
+            p2 = nodes[segment].position;
+            p3 = nodes[segment + 1].position;
+            p4 = nodes[segment + 2].position;
+        }
+
+        float t2 = ratio * ratio;
+        float t3 = t2 * ratio;
+
+        // Equation for a catmull-Rom spline
+        float x = 0.5f * ((2.0f * p2.x) + (-p1.x + p3.x) * ratio + (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) * t2 + (-p1.x + 3.0f * p2.x - 3.0f * p3.x + p4.x) * t3);
+        float y = 0.5f * ((2.0f * p2.y) + (-p1.y + p3.y) * ratio + (2.0f * p1.y - 5.0f * p2.y + 4 * p3.y - p4.y) * t2 + (-p1.y + 3.0f * p2.y - 3.0f * p3.y + p4.y) * t3);
+        float z = 0.5f * ((2.0f * p2.z) + (-p1.z + p3.z) * ratio + (2.0f * p1.z - 5.0f * p2.z + 4 * p3.z - p4.z) * t2 + (-p1.z + 3.0f * p2.z - 3.0f * p3.z + p4.z) * t3);
+
+        return new Vector3(x, y, z);
+    }
+
+    public Quaternion CurrentSegmentAngle(int segment)
+    {
+        return (nodes[segment].rotation);
+    }
+}
