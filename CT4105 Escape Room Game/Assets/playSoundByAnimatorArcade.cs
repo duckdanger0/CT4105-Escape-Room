7780dfad462c2cd0b7496d85401e347b958e5ebe
duckdanger0 <77931413+duckdanger0@users.noreply.github.com>
2021-05-19 18:47:30 +0100

    Another Script

---
+++ CT4105 Escape Room Game/Assets/playSoundByAnimatorArcade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundByAnimatorArcade : MonoBehaviour
+{
+    public AudioClip openDoorSound;
+    public AudioClip closeDoorSound;
+    public AudioSource speaker;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        speaker = GetComponent<AudioSource>();
+    }
+
+    void playOpenDoorSound()
+    {
+        speaker.PlayOneShot(openDoorSound, 1f);
+    }
+    void playCloseDoorSound()
+    {
+        speaker.PlayOneShot(closeDoorSound, 1f);
+    }
+
+
+}
