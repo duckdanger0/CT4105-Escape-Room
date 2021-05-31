74d7ea1000d3c292eeea0f9a245c1a1dd634d06f
MLDanny <67206020+MLDanny@users.noreply.github.com>
2021-05-31 16:05:37 +0100

    jumpscare implementation

---
+++ CT4105 Escape Room Game/Assets/Scripts/Jumpscare.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Jumpscare : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private GameObject jumpscare;
+
+    public AudioSource Audio;
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Audio.Play();
+            jumpscare.SetActive(true);
+
+        }
+    }
+}
